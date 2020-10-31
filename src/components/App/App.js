c6de06dd1c12820af70c7845529430e9726fa88c
Beau Weise <beauweise@gmail.com>
2020-10-31 12:53:15 -0500

    added routers

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,14 +9,19 @@ import PageOne from '../PageOne/PageOne';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Feedback!</h1>
-          <h4><i>Don't forget it!</i></h4>
-        </header>
-        <br/>
-        <PageOne/>
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Feedback!</h1>
+            <h4><i>Don't forget it!</i></h4>
+          </header>
+          <br />
+          <Route exact path="/" component={PageOne} />
+          <Route exact path="/PageTwo" component={PageTwo} />
+          <Route exact path="/PageThree" component={PageThree} />
+          <Route exact path="/PageFour" component={PageFour} />
+        </div>
+      </Router>
     );
   }
 }
