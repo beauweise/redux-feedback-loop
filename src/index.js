afbc4f6fe6ff16d37b86242d09b11c872d03200d
Beau Weise <beauweise@gmail.com>
2020-11-01 10:21:53 -0600

    added applymiddleware

--- src/index.js
+++ src/index.js
@@ -4,17 +4,26 @@ import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from'redux';
+import { createStore, combineReducers, applyMiddleware } from'redux';
+import logger from 'redux-logger';
 
-// pageOneReducer = (state=[], action)=>{
 
-// }
+
+const pageOneReducer = (state=[], action) => {
+    if(action.type === 'SET_PIZZA_MENU'){
+        console.log('payload', action.payload);
+        return action.payload
+    }
+    return state;
+}
 
 
 const storeInstance = createStore(
     combineReducers({
-    //    pageOneReducer,
-    })
+       pageOneReducer,
+    }),
+  applyMiddleware(logger)
+
 );
 ReactDOM.render(<Provider store={storeInstance}> <App /> </Provider>, document.getElementById('root'));
 registerServiceWorker();
